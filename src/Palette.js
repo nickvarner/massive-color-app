2649fb9ab1fbaa7f2d9302975ea32262a6803647
[nick varner] <nick.varner@gmail.com>
2021-02-02 10:23:41 -0800

    first commit

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* navbar goes here */}
+                <div className="Palette-colors">
+                    {/* bunch of color boxes */}
+                </div>
+                {/* footer eventually */}
+            </div>
+        )
+    }
+}
+
+export default Palette;
