a3e1253da284bd3787eb68053ce9f2c98c096fef
[nick varner] <nick.varner@gmail.com>
2021-02-03 14:00:00 -0800

    added snackbar and styled select

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,56 +3,77 @@ import Select from "@material-ui/core/Select"
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css'
 import './Navbar.css'
-import { MenuItem } from '@material-ui/core';
+import { MenuItem, Snackbar } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 class Navbar extends Component {
     constructor(props) {
         super(props);
-        this.state = { format: 'hex' }
-        this.handleChange = this.handleChange.bind(this);
+        this.state = { format: 'hex', open: false }
+        this.handleFormatChange = this.handleFormatChange.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this)
     }
-    handleChange(evt) {
-        this.setState({ format: evt.target.value });
+    handleFormatChange(evt) {
+        this.setState({ format: evt.target.value, open: true });
         this.props.handleChange(evt.target.value);
+    }
+    closeSnackbar() {
+        this.setState({ open: false })
     }
     render() {
         const {level, changeLevel } = this.props;
         const {format} = this.state;
         return (
-            <header className="NavBar">
+            <header className="Navbar">
                 <div className="logo">
                     <a href="#">reactcolorpicker</a>
-                        <div className="slider-container">
-                            <span>Level: {level}</span>
-                            <div className="slider">
-                            <Slider
-                                defaultValue={level}
-                                min={100}
-                                max={900}
-                                onAfterChange={changeLevel}
-                                step={100}
-                                trackStyle={{ backgroundColor: 'transparent' }}
-                                handleStyle={{
-                                    borderColor: 'green',
-                                    height: 13,
-                                    width: 13,
-                                    marginLeft: -7,
-                                    marginTop: -3,
-                                    backgroundColor: 'green',
-                                    boxShadow: 'none'
-                                }}
-                                railStyle={{height: 8}}
-                                />   
-                            </div>
-                        </div>
-                        <div className="select-container">
-                            <Select value={format} onChange={this.handleChange} >
-                                <MenuItem value="hex">hex - #ffffff</MenuItem>
-                                <MenuItem value="rgb">rgb(255,255,255)</MenuItem>
-                                <MenuItem value="rgba">rgba(255,255,255,1.0)</MenuItem>
-                            </Select>
-                        </div>
                 </div>
+                    <div className="slider-container">
+                        <span>Level: {level}</span>
+                        <div className="slider">
+                        <Slider
+                            defaultValue={level}
+                            min={100}
+                            max={900}
+                            onAfterChange={changeLevel}
+                            step={100}
+                            trackStyle={{ backgroundColor: 'transparent' }}
+                            handleStyle={{
+                                borderColor: 'green',
+                                height: 13,
+                                width: 13,
+                                marginLeft: -7,
+                                marginTop: -3,
+                                backgroundColor: 'green',
+                                boxShadow: 'none'
+                            }}
+                            railStyle={{height: 8}}
+                            />   
+                        </div>
+                    </div>
+                    <div className="select-container">
+                        <Select value={format} onChange={this.handleFormatChange} >
+                            <MenuItem value="hex">hex - #ffffff</MenuItem>
+                            <MenuItem value="rgb">rgb(255,255,255)</MenuItem>
+                            <MenuItem value="rgba">rgba(255,255,255,1.0)</MenuItem>
+                        </Select>
+                    </div>
+                    <Snackbar 
+                        anchorOrigin={{ vertical: "bottom", horizontal: "left"}} 
+                        open={this.state.open} 
+                        autoHideDuration={3000} 
+                        message={<span id="message id">format changed to {format}</span>}
+                        ContentProps={{
+                            "aria-describedby": "message-id"
+                        }}
+                        onClose={this.closeSnackbar}
+                        action={[
+                            <IconButton onClick={this.closeSnackbar} color="inherit" key="close" aria-label="close" >
+                                <CloseIcon />
+                            </IconButton>
+                        ]}
+                    />
             </header>
         )
     }
