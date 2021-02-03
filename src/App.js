d411932be474782ce54a4f36f7201ae70e65de9f
[nick varner] <nick.varner@gmail.com>
2021-02-03 10:34:38 -0800

    added color helper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Palette from './Palette'
 import seedColors from './seedColors';
-
+import { generatePalette } from './ColorHelpers'
 const App = () => {
+  console.log(generatePalette(seedColors[4]))
   return (
     <div className="App">
       <Palette {...seedColors[4]} />
