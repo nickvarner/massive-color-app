12b48cdd909c1e65150b8c57e7b189f655263023
[nick varner] <nick.varner@gmail.com>
2021-02-04 12:26:31 -0800

    added link to palette

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" render={() => <PaletteList palettes={seedColors} />} />
+        <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
         <Route exact path="/palette/:id" render={routeProps => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))} /> }/>
       </Switch>
     </div>
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component, useState } from 'react'
+import {Link, withRouter} from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
     root: {
         backgroundColor: "blue",
-        height: "100%",
+        height: "100vh",
         display: "flex",
         alignItems: "flex-start",
         justifyContent: "center"
@@ -34,25 +34,28 @@ const styles = {
     }
 }
 
-const PaletteList = (props) => {
-    // const [somethingState, setSomethingState] = useState('default state')
-    const {classes, palettes} = props;
-    return (
-        <div className={classes.root}>
+class PaletteList extends Component {
+    goToPalette(id) {
+        this.props.history.push(`/palette/${id}`)
+    }
+    render() {
+        const {classes, palettes} = this.props;
+        console.log(this.props);
+        return (
+            <div className={classes.root}>
             <div className={classes.container}>
                 <nav className={classes.nav}>
                     <h1>react colors</h1>
                 </nav>
                 <div className={classes.palettes}>
                 {palettes.map(palette => (
-                    <Link to={`/palette/${palette.id}`}>
-                        <MiniPalette {...palette} />
-                    </Link>
+                        <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)} />
                 ))}
                 </div>
             </div>
         </div>
-    )
+        )
+    }
 }
 
 export default withStyles(styles)(PaletteList)
