2fa87ec48ea8c05efe8221f9ea667cd39b177bf7
[nick varner] <nick.varner@gmail.com>
2021-02-04 10:13:51 -0800

    added materialui styles and minipalette styling

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,13 +1,15 @@
 import {Link} from 'react-router-dom';
+import MiniPalette from './MiniPalette';
 
 const PaletteList = (props) => {
     const {palettes} = props;
     return (
         <div>
+            <MiniPalette />
             <h1>react colors</h1>
             {palettes.map(palette => (
                 <Link to={`/palette/${palette.id}`}>
-                    {palette.paletteName}
+                    <MiniPalette {...palette} />
                 </Link>
             ))}
         </div>
