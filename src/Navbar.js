3c2bc8d87ab4747b5f109b67e35fa83805a5684c
[nick varner] <nick.varner@gmail.com>
2021-02-05 11:41:43 -0800

    add navbar and footer to palette and singlecolorpalette

--- src/Navbar.js
+++ src/Navbar.js
@@ -23,14 +23,14 @@ class Navbar extends Component {
         this.setState({ open: false })
     }
     render() {
-        const {level, changeLevel } = this.props;
+        const {level, changeLevel, showingAllColors } = this.props;
         const {format} = this.state;
         return (
             <header className="Navbar">
                 <div className="logo">
                     <Link to="/">reactcolorpicker</Link>
                 </div>
-                    <div className="slider-container">
+                    {showingAllColors && (<div className="slider-container">
                         <span>Level: {level}</span>
                         <div className="slider">
                         <Slider
@@ -52,7 +52,7 @@ class Navbar extends Component {
                             railStyle={{height: 8}}
                             />   
                         </div>
-                    </div>
+                    </div>)}
                     <div className="select-container">
                         <Select value={format} onChange={this.handleFormatChange} >
                             <MenuItem value="hex">hex - #ffffff</MenuItem>
---
+++ src/PaletteFooter.js
@@ -0,0 +1,13 @@
+
+
+const PaletteFooter = (props) => {
+    const {paletteName, emoji} = props;
+    return (
+    <footer className="Palette-footer">
+        {paletteName}
+        <span className="emoji">{emoji}</span>
+    </footer>
+    )
+}
+
+export default PaletteFooter;
