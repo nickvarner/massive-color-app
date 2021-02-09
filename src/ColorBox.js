3f1021fb28a5e772c0d2d1fbd747640febfbfb7d
[nick varner] <nick.varner@gmail.com>
2021-02-09 10:02:49 -0800

    adjust text color based on chroma contrast

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {CopyToClipboard} from "react-copy-to-clipboard";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
 import "./ColorBox.css"
 
 class ColorBox extends Component {
@@ -8,15 +9,24 @@ class ColorBox extends Component {
         super(props);
         this.state = { copied: false }
         this.changeCopyState = this.changeCopyState.bind(this);
+        this.contrastCheck = this.contrastCheck.bind(this);
     }
     changeCopyState() {
         this.setState({ copied: true }, () => {
             setTimeout(() => this.setState({ copied: false }), 1500)
         });
     }
+    contrastCheck (color) {
+        if(chroma.contrast(color, "black") < 6) {
+            return "white"
+        } else {
+            return "rgba(0,0,0,0.5)"
+        }
+    }
     render() {
         const {name, background, id, paletteId, showLink} = this.props;
         const {copied} = this.state;
+        const isContrast = this.contrastCheck(background)
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div className="ColorBox" style={{ background }}>
@@ -26,17 +36,17 @@ class ColorBox extends Component {
                     />
                     <div className={`copy-msg ${copied && "show"}`}>
                         <h1>copied</h1>
-                        <p>{this.props.background}</p>
+                        <p style={{ color: isContrast }} >{this.props.background}</p>
                     </div>
                     <div className="copy-container">
                         <div className="box-content">
-                            <span>{name}</span>
+                            <span style={{ color: isContrast }} >{name}</span>
                         </div>
-                        <button className="copy-button" >copy</button>
+                        <button className="copy-button" style={{ color: isContrast }}>copy</button>
                     </div>
                     {showLink && (
                         <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}>
-                            <span className="see-more">more</span>
+                            <span className="see-more" style={{ color: isContrast }}>more</span>
                         </Link>
                     )}
                 </div>
